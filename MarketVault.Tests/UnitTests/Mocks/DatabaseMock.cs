00a8c8d043b29978abd7ad7f465d5299bea7afe7
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-03-28 19:49:16 +0200

    Fully implemented css to current views, view changes, succesfully built testing infrastructure project

---
+++ MarketVault.Tests/UnitTests/Mocks/DatabaseMock.cs
@@ -0,0 +1,24 @@
+﻿namespace MarketVault.Tests.UnitTests.Mocks
+{
+    using MarketVault.Data;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Static class used for getting a mock with the Database
+    /// </summary>
+    public static class DatabaseMock
+    {
+        public static ApplicationDbContext Mock
+        {
+            get
+            {
+                var dbContextOptions = new
+                    DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase("MarketVaultDb" + DateTime.Now)
+                    .Options;
+
+                return new ApplicationDbContext(dbContextOptions);
+            }
+        }
+    }
+}
